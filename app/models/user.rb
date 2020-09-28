4ca32b10b4c75cc0117ab27beb0541cdddde93a0
gurbuzertunga <gurbuzertunga@gmail.com>
2020-09-28 16:39:25 +0300

    created the users controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-    validates :username, presence :true, uniqueness :true
-    validates :email, presence :true
-    validates :password, presence :true
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true
+    validates :password, presence: true
+
+    
 end
